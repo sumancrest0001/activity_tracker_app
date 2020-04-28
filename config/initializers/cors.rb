fa052090a702701f468e18a4aa5c5b8203270fce
sumancrest0001 <suman.crest0001@gmail.com>
2020-04-28 23:09:05 +0545

    fixed syntax error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do 
     origins "https://activity-tracker-suman.herokuapp.com"
-    resource "*", headers: :any, methods; [:get, :post, :put, :patch, :delete, :options, :head],
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
 end

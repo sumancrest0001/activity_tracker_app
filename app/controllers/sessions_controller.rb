421babfb9be27ef94149c97422ab4be7e089cebd
sumancrest0001 <suman.crest0001@gmail.com>
2020-04-28 22:46:55 +0545

    made the API running on localhost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User
-      .find_by(email: params['user'])
+      .find_by(email: params['user']['email'])
       .try(:autneticate, params['user']['password'])
     if user
       session[:user_id] = user.id

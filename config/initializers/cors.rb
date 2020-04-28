5a311e4ca7d03aa0c86611c3a877d00bedde4801
sumancrest0001 <suman.crest0001@gmail.com>
2020-04-28 22:59:43 +0545

    changed config file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     credentials: true
   end
 
-  # allow do 
-    # origins "http://localhost:3000"
-    # resource "*", headers: :any, methods; [:get, :post, :put, :patch, :delete, :options, :head],
-    # credentials: true
-  # end
+  allow do 
+    origins "https://activity-tracker-suman.herokuapp.com"
+    resource "*", headers: :any, methods; [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api", domain: "daily-routine-tracker.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api", domain: "track-my-activity.herokuapp.com"
 else 
   Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api"
 end

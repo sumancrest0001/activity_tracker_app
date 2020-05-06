d747166f7bac6c578fc9c1bc207a7c6cd1a9aeea
sumancrest0001 <suman.crest0001@gmail.com>
2020-05-07 01:19:01 +0545

    added strict rule for same_site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allowed_methods = %i[ get post put patch delete options head ]
   allow do 
     origins "http://localhost:3000"
-    resource "*", headers: :any, methods: allowed_methods,
+    resource "*", headers: :any,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
 
   allow do 
     origins "https://suman-activity-tracker.herokuapp.com/"
-    resource "*", headers: :any, methods: allowed_methods,
+    resource "*", headers: :any,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api", domain: "track-my-activity.herokuapp.com"
+  Rails.application.config.session_store :cookie_store,
+  key: "_activity_tracker_api",
+  domain: "track-my-activity.herokuapp.com",
+  secure: true,
+  same_site: :strict
 else 
   Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api"
 end

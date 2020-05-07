467c5ad221980089e3cc12dbba5fa0f45b273e50
sumancrest0001 <suman.crest0001@gmail.com>
2020-05-07 09:30:46 +0545

    changed session and cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   
   allow do
-    origins 'https://suman-activity-tracker.herokuapp.com/'
+    origins 'https://suman-activity-tracker.herokuapp.com'
 
     resource '*',
       headers: :any,

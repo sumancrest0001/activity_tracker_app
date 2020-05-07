3f5e80c0f7226c0a97803cbc064e1bc1eb4926b1
sumancrest0001 <suman.crest0001@gmail.com>
2020-05-07 17:27:28 +0545

    fixed linter error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
   key: '_track_my_activity_api',
-  domain: 'track-my-activity.herokuapp.com',
-
+  domain: 'track-my-activity.herokuapp.com'
 else 
   Rails.application.config.session_store :cookie_store, key: '_activity_tracker_api'
 end

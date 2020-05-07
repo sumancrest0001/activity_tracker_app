e714302453d52b6110d1adaac25e7932fa73c7dd
sumancrest0001 <suman.crest0001@gmail.com>
2020-05-07 17:40:11 +0545

    changed session file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store,
-  key: '_track_my_activity_api',
-  domain: 'track-my-activity.herokuapp.com'
-else 
-  Rails.application.config.session_store :cookie_store, key: '_activity_tracker_api'
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api", domain: "track-my-activity.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_activity_tracker_api"
 end

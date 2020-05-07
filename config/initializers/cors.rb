80b3397c5dfa30fcf8fd1ca5b7b8fe4ac8cb8fb7
sumancrest0001 <suman.crest0001@gmail.com>
2020-05-07 09:08:59 +0545

    changed cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,20 @@
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do 
-    origins "http://localhost:3000"
-    resource "*", headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
+  allow do
+    origins 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  
+  allow do
+    origins 'https://suman-activity-tracker.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
